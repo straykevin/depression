d995f031abadbd87a16eb34dcc4d739328904782
Andrew Mayhew <91130138+amayhew2@users.noreply.github.com>
2024-01-27 02:11:30 -0500

    Create lighting.lua
    Added the lighting module

---
+++ src/StarterPlayerScripts/lighting.lua
@@ -0,0 +1,22 @@
+--Services
+local Lighting = game:GetService("Lighting")
+local TweenService = game:GetService("TweenService")
+
+local LightingModule = {}
+
+--Presets for lighting (Name each the actual attribute that you'd like to update)
+local levelSettings = {
+    [1] = {Ambient = Color3.new(), Brightness = Color3.new(), EnvironmentDiffusion = 1}
+}
+
+--Updates the bloom
+function LightingModule.setBloom(threshold : number, speed : number)
+    TweenService:Create(game.Lighting.Bloom, TweenInfo.new(speed), {Threshold = threshold}):Play()
+end
+
+--Updates the lighting to one of the presets
+function LightingModule.updateZoneLighting(presetNum : number)
+    for key, value in pairs(levelSettings[presetNum]) do
+        Lighting[key] = value
+    end
+end
